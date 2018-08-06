a1df9bd1482b43907f15f8f9971972421be18d85
kincsesbalint <kincsesbalint@gmail.com>
2018-08-06 10:17:48 +0200

    ex_fmprobe for presentation

--- scripts/ex_fmprobe.py
+++ scripts/ex_fmprobe.py
@@ -31,7 +31,10 @@ reorient_func = pe.MapNode(fsl.utils.Reorient2Std(),
                       iterfield=['in_file'],
                       name="reorient_func")
 
-myfm=fm.fieldmapper()
+myfm=fm.fieldmapper(TE1=4.9,
+                TE2=7.3,
+                dwell_time=0.00035,
+                    unwarp_direction="y-")
 
 totalWorkflow = nipype.Workflow('fm_probe')
 totalWorkflow.base_dir = '.'
