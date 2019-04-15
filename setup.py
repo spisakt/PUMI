854e083fceee06aac7cdd1fdf3186147974ce76a
Tamas Spisak <spisak.tms@gmail.com>
2019-04-15 16:28:27 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='PUMI',
+    version='v0.1',
+    packages=['', 'plot', 'utils', 'scripts', 'porcupine', 'anat_preproc', 'connectivity', 'func_preproc',
+              'func_preproc.info'],
+    url='https://github.com/spisakt/PUMI',
+    license='BSD-3',
+    author='tspisak',
+    author_email='tamas.spisak@uk-essen.de',
+    description='Pypelines Utilizing a Modular Inventory'
+)
